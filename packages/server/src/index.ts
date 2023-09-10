ca01f47706376cad2e54f31b88b9234bbae6d712
BhavyaCodes <xbhavyatomar@gmail.com>
2023-09-10 16:10:23 +0530

    add validation

--- packages/server/src/index.ts
+++ packages/server/src/index.ts
@@ -1,14 +1,59 @@
 import { Hono } from "hono";
-
 import Redis from "ioredis";
+import { validator } from "hono/validator";
+import { HTTPException } from "hono/http-exception";
 
 const redis = new Redis();
 
-const app = new Hono();
+const api = new Hono();
 
-app.get("/", async (c) => {
+api.get("/", async (c) => {
   const color = await redis.get("color");
   return c.json({ color });
+});
+
+api.get(
+  "/:number",
+  validator("param", (value, c) => {
+    const numberString = value.number;
+    if (!numberString) {
+      throw new HTTPException(400, { message: "Invalid number" });
+    }
+    const number = Math.floor(parseInt(numberString));
+    console.log(number);
+
+    if (number < 0 || isNaN(number)) {
+      throw new HTTPException(400, { message: "Invalid number" });
+    }
+
+    if (number > 50) {
+      throw new HTTPException(400, {
+        message: "number too high, use less than 50",
+      });
+    }
+
+    return {
+      number: number.toString(),
+    };
+  }),
+  async (c) => {
+    const numberString = c.req.valid("param").number;
+    return c.text(numberString);
+  }
+);
+
+const app = new Hono();
+app.route("/api", api);
+
+app.onError((err, c) => {
+  if (err instanceof HTTPException) {
+    // Get the custom response
+    return err.getResponse();
+  }
+
+  c.status(500);
+  return c.text("internal server error");
+  //...
 });
 
 export default {
