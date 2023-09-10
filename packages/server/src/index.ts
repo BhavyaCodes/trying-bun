5e2e03294dd5fcb368707e80d74206a4c0871280
BhavyaCodes <xbhavyatomar@gmail.com>
2023-09-10 13:29:46 +0530

    install ioredis

--- packages/server/src/index.ts
+++ packages/server/src/index.ts
@@ -1,8 +1,15 @@
 import { Hono } from "hono";
 
+import Redis from "ioredis";
+
+const redis = new Redis();
+
 const app = new Hono();
 
-app.get("/", (c) => c.text("Hello Hono!"));
+app.get("/", async (c) => {
+  const color = await redis.get("color");
+  return c.json({ color });
+});
 
 export default {
   port: 5000,
