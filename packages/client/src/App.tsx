a697900a28859f0144a4d925ed62f9331ca4b86a
BhavyaCodes <xbhavyatomar@gmail.com>
2023-09-13 22:32:21 +0530

    add some styling

--- packages/client/src/App.tsx
+++ packages/client/src/App.tsx
@@ -1,12 +1,17 @@
 import axios from "axios";
 import { useEffect } from "react";
+import classes from "./App.module.css";
 
 function App() {
   useEffect(() => {
     axios.get("/api/4").then((res) => console.log(res.data));
   });
 
-  return <div>hi</div>;
+  return (
+    <main className={classes.app}>
+      <h1 className={classes.heading}>Bun Fibonacci Calculator</h1>
+    </main>
+  );
 }
 
 export default App;
